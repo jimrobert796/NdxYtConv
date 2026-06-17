a2b1488e88bdceb2d3bd1a51d1cadc08f064d126
jimrobert796 <jimmyroberto270@gmail.com>
2026-06-17 00:06:03 -0600

    WEB_EMBED test

--- ytconver/core/downloader.py
+++ ytconver/core/downloader.py
@@ -65,7 +65,7 @@ class YouTubeDownloaderCore:
     def get_video_info(self, url: str) -> Optional[VideoInfo]:
         """Obtiene información del video y detecta si es auto-generated"""
         try:
-            yt = YouTube(url, client='WEB', use_po_token=True)
+            yt = YouTube(url, client='WEB_EMBED', use_po_token=True)
             
             # Verificar si es auto-generated
             is_auto_generated = self._is_auto_generated(yt)
